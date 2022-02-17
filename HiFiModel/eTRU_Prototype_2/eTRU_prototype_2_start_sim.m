d357d57dfe8a567330e7ae7954b96f6205c4dc83
Kresten Kjær Sørensen <kresten.soerensen@bitzerdk.com>
2022-02-17 11:18:34 +0100

    Simulation runing

--- HiFiModel/eTRU_Prototype_2/eTRU_prototype_2_start_sim.m
+++ HiFiModel/eTRU_Prototype_2/eTRU_prototype_2_start_sim.m
@@ -6,11 +6,13 @@ global Ref
 
 ambient_temp = 20;
 
-box_start_temp = 20;
+box_start_temp = 10;
+
+im_start_temp = (ambient_temp + box_start_temp)/2;
 
 start_pressure_high = Ref.PDewT(ambient_temp)
-start_pressure_im = Ref.PDewT(ambient_temp) + 1
-start_pressure_low = Ref.PDewT(box_start_temp) + 2
+start_pressure_im = Ref.PDewT(im_start_temp)
+start_pressure_low = Ref.PDewT(box_start_temp)
 
 cargo_start_temp = box_start_temp;
 cargo_mass = 1000;
