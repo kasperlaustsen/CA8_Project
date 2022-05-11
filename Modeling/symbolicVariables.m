929713f17cda2d2d43244632f19abe3c8c5c9192
kasperlaustsen <kasperlaustsen@gmail.com>
2022-05-11 11:48:21 +0200

    Commented out all mdots in symbolicVariables that were had a name after it. Fx m_dot_2_pjj. This is to comply with constraintsNew. m_dot_5_FT was changed to m_dot_5 in collecting components 01, in the state equations

--- Modeling/symbolicVariables.m
+++ Modeling/symbolicVariables.m
@@ -10,14 +10,14 @@ syms m_dot_1 m_dot_2 m_dot_4 m_dot_5
 
 % variables in f
 syms M_PJJ M_Con T_m Q_rm Q_ma  Cp_m m_dot_air m_dot_bar_air T_mlv Q_amlv 
-syms Q_mlv Q_mvmlv sigma T_mv Q_amv Q_mv M_lv M_v  m_dot_dew 
+syms Q_mlv Q_mvmlv sigma T_mv Q_amv Q_mv M_lv M_v m_dot_dew 
 
 % pipe joining junction
-syms m_dot_2_PJJ
+syms % m_dot_2_PJJ
 
 % compressor 2
 syms V_1_COM2 V_C_COM2 v_1_COM2 v_2_COM2 omega p_i1_COM2 p_i2_COM2 gamma 
-syms kl_1 kl_2 C_cp C_cvhtp syms m_dot_2_COM2 gamma2
+syms kl_1 kl_2 C_cp C_cvhtp syms  gamma2 %m_dot_2_COM2
 
 % compressor 1
 syms V_1_COM1 V_C_COM1 v_1_COM1 v_2_COM1 omega p_i1_COM1 p_i2_COM1 gamma 
@@ -25,14 +25,14 @@ syms kl_1 kl_2 C_cp C_cv gamma1
 
 % condenser
 syms Q_rm lambda M_Con M_m_Con V_i_Con v_Con UA_rm T_r T_m UA_ma T_ambi 
-syms U_fan_1 m_dot_3_Con	
+syms U_fan_1 %m_dot_3_Con	
 
 % valve
 syms Theta_1 Theta_2 K_Val v_CTV_in v_EV_in fp
-syms m_dot_3_Val m_dot_5_Val
+syms % m_dot_3_Val m_dot_5_Val
 
 % flash tank
-syms N M m_dot_5_FT
+syms N M 
 
 % evaporator 
 syms sigma v_Eva V_i_Eva T_retfan T_ret Q_fan_2 m_dot_air Cp_air T_retsh 
